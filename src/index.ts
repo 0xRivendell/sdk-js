1596145a20d5ecb76379017c797c4b676c8f1b42
Shlok D. Mange <shlok_mange@protonmail.com>
2023-08-13 14:52:15 +0530

    v0

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,23 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
-
 class Intents {
 	
 	private apiKey: string
 	public apiUrl: string
 
 	constructor(apiKey: string) {
-		this.apiKey = apiKey;
-		this.apiUrl = 'https://intents-api.onrender.com/';
+		this.apiKey = apiKey
+		this.apiUrl = 'https://intents-api.onrender.com'
 	}
 
 	public getTransaction = async (chainId: string, command: string, signer: string) => {
-		const res = await fetch(`${this.apiUrl}/payments`, {
+		console.log(this.apiKey)
+		const res = await fetch(`${this.apiUrl}/intents`, {
 			body: JSON.stringify({
 				chainId, command, recipient: signer
 			}),
 			headers: {
 				'Content-Type': 'application/json',
-			}
+			},
+			method: 'POST',
 		})
 		const json = await res.json()
 		return json
---
+++ test/api.test.ts
@@ -0,0 +1,13 @@
+const { Intents } = require('../dist/index.js')
+
+const intents = new Intents('test-api-key')
+
+let command = `I want to send 0.001 eth to shlok28.eth`
+let signer = `0x03f142529a7B70305C07a50fAA44f6EBDADB4624`
+
+const main = async () => {
+    const res = await intents.getTransaction('1', command, signer)
+    console.log(res)
+}
+
+main()
