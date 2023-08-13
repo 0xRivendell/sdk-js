f65489d1e8ab0f7a66ec4668ee187a23db47b46b
Shlok D. Mange <shlok_mange@protonmail.com>
2023-08-13 13:02:43 +0530

    get transaction func

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,25 @@ export const sum = (a: number, b: number) => {
 class Intents {
 	
 	private apiKey: string
-		
+	public apiUrl: string
+
 	constructor(apiKey: string) {
 		this.apiKey = apiKey;
-  	}
+		this.apiUrl = 'https://intents-api.onrender.com/';
+	}
+
+	public getTransaction = async (chainId: string, command: string, signer: string) => {
+		const res = await fetch(this.apiUrl, {
+			body: JSON.stringify({
+				chainId, command, recipient: signer
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		})
+		const json = await res.json()
+		return json
+	}
 
 }
 
