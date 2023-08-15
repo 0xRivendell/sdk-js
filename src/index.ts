0f1c033bbde15a6bc83b283365c0985c7e4621d8
Shlok D. Mange <shlok_mange@protonmail.com>
2023-08-15 17:33:18 +0530

    update api url from render -> bytekode.xyz

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@ class Intents {
 
 	constructor(apiKey: string) {
 		this.apiKey = apiKey
-		this.apiUrl = 'https://intents-api.onrender.com'
+		this.apiUrl = 'https://intents-api.bytekode.xyz'
 	}
 
 	public getTransaction = async (chainId: string, command: string, signer: string) => {
 		console.log(this.apiKey)
-		const res = await fetch(`${this.apiUrl}/intents`, {
+		const res = await fetch(`${this.apiUrl}/payments`, {
 			body: JSON.stringify({
 				chainId, command, recipient: signer
 			}),
