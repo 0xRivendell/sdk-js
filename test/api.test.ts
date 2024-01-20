97333526f588b49bd63e1148c2961acdeeec6d4f
Shlok <shlok_mange@protonmail.com>
2024-01-20 15:53:31 +0530

    something something

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,9 +1,5 @@
 const { Riv } = require('../dist/index.js')
-
 const riv = new Riv('test-api-key')
-
-let command = 'Transfer 0.001 eth to shlok28.eth'
-let signer = `0x7e8eb4FbA4e412d39d95618Ad6d735495baf6e9a`
 
 const main = async () => {
     const res = await riv.stake({ chainId: '1', amount: '0.001' })
