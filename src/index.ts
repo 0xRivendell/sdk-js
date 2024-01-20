48e1e30c37e970fe8b5bd57b848d7075e7f747f7
Shlok <shlok_mange@protonmail.com>
2024-01-20 15:34:26 +0530

    updated api url

--- src/index.ts
+++ src/index.ts
@@ -7,31 +7,31 @@ class Intents {
 
 	constructor(apiKey: string) {
 		this.apiKey = apiKey
-		this.apiUrl = 'https://intents-api.onrender.com'
+		this.apiUrl = 'https://rivendell-api.onrender.com'
 	}
 
-	public getTransaction = async (chainId: string, command: string, signer: string) => {
-		// console.log(this.apiKey)
-		const res = await fetch(`${this.apiUrl}/payments`, {
-			body: JSON.stringify({
-				chainId, command, recipient: signer
-			}),
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			method: 'POST',
-		})
-		const json = await res.json()
-		return json
-	}
+	// public getTransaction = async (chainId: string, command: string, signer: string) => {
+	// 	// console.log(this.apiKey)
+	// 	const res = await fetch(`${this.apiUrl}/payments`, {
+	// 		body: JSON.stringify({
+	// 			chainId, command, recipient: signer
+	// 		}),
+	// 		headers: {
+	// 			'Content-Type': 'application/json',
+	// 		},
+	// 		method: 'POST',
+	// 	})
+	// 	const json = await res.json()
+	// 	return json
+	// }
 
-	public swap = async (swapObject: ISwap) => {
+	// public swap = async (swapObject: ISwap) => {
 
-	}
+	// }
 
-	public bridge = async (fromChain: string, toChain: string, token: string, amount: string, from: string) => {
+	// public bridge = async (fromChain: string, toChain: string, token: string, amount: string, from: string) => {
 
-	}
+	// }
 
 	public stake = async ({ chainId, amount }: IStake) => {
 		const res = await fetch(`${this.apiUrl}/stake`, {
@@ -45,4 +45,4 @@ class Intents {
 	
 }
 
-export { Intents }
+export { Intents, IStake }
