a4aa4f819a5561d83b6bbcb4b2a5fecc9f9ec8b1
Shlok <shlok_mange@protonmail.com>
2024-01-20 15:52:28 +0530

    updated api example

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,12 +1,12 @@
-const { Intents } = require('../dist/index.js')
+const { Riv } = require('../dist/index.js')
 
-const intents = new Intents('test-api-key')
+const riv = new Riv('test-api-key')
 
 let command = 'Transfer 0.001 eth to shlok28.eth'
 let signer = `0x7e8eb4FbA4e412d39d95618Ad6d735495baf6e9a`
 
 const main = async () => {
-    const res = await intents.getTransaction('80001', command, signer)
+    const res = await riv.stake({ chainId: '1', amount: '0.001' })
     console.log(res)
 }
 
